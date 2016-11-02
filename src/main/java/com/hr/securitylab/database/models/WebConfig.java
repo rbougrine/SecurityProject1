c1437777782a7a185dccf0667cdf8f4733ab3d69
randa <rrbougrine@gmail.com>
2016-11-02 11:58:52 +0100

    Added working resetpassword page, cleaned a few files up

--- src/main/java/com/hr/securitylab/database/models/WebConfig.java
+++ src/main/java/com/hr/securitylab/database/models/WebConfig.java
@@ -4,14 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 @Configuration
 public class LoginDB extends WebSecurityConfigurerAdapter
 {
@@ -24,8 +24,9 @@ public class LoginDB extends WebSecurityConfigurerAdapter
     protected void configure(HttpSecurity http) throws Exception
     {
                 http
+
                     .authorizeRequests()
-                    .antMatchers("/finddevice","/login", "/register", "/resetpassword").permitAll()
+                    .antMatchers("/login", "/register", "/resetpassword").permitAll()
                     .anyRequest().authenticated()
                 .and()
                      .formLogin()
---
+++ src/main/java/com/hr/securitylab/validationProcess/annotations/ValidQuestion.java
@@ -0,0 +1,7 @@
+package com.hr.securitylab.validationProcess.annotations;
+
+/**
+ * Created by Randa on 1-11-2016.
+ */
+public class ValidQuestion {
+}
